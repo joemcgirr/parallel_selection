48360e1218f4136d0dcb19226d38a33c46e9c722
joemcgirr <46727259+joemcgirr@users.noreply.github.com>
2019-10-31 15:04:15 -0400

    Update parallel_selection.R

--- parallel_selection.R
+++ parallel_selection.R
@@ -1,14 +1,34 @@
 ######################
-# parallel selection #
+# parallel selection -------------------------------------------------------------------------------
 ######################
+#
 # Use this script to identify and visualize genes shwoing
 # signs of a hard selective sweep at user defined thresholds
-
-
+#
+####################################################
 # 1. set working directory containing:
 #
 #     SweeD_Report.SPECIES_NAME_grid1000_sweeps.txt
-#          
+#     ProteinTable_SPECIES_NAME.csv"
+#
+####################################################
+
+setwd("/path/to/wrkdir")
+#setwd("C:/Users/jmcgirr/Desktop/git_test/")
+
+####################################################
+# 2. Set SPECIES_NAMES (prefixes above)
+####################################################
+
+species_names <- c("species1","species2")
+#species_names <- c("histoplasmaG186ar","Neoformans")
+
+####################################################
+# 3. Set CLR percentile threshold
+####################################################
+
+clr_thresh <- 0.9
+
 
 required_packages <- c("BiocManager","tidyverse","plyranges","seqnir","UpSetR")
 
@@ -22,7 +42,6 @@ library(tidyverse)
 library(plyranges)
 library(seqinr)
 library(UpSetR)
-
 
 
 for (species_name in species_names){
@@ -41,17 +60,17 @@ for (species_name in species_names){
   
   
   # get best candidate for each chromosome
-  #candidate_outliers <- sweed %>% 
+  #  candidate_outliers <- sweed %>% 
   #  group_by(seqnames) %>%
   #  mutate(highest_CLR = max(CLR)) %>%
   #  group_by(seqnames) %>%
   #  filter(CLR == highest_CLR) %>%
   #  as.data.frame()
   
-  # get top 90 percentile for each chromosome
-  candidate_outliers <- sweed %>% 
+  # get top "clr_thresh" percentile for each chromosome
+    candidate_outliers <- sweed %>% 
     group_by(seqnames) %>%
-    mutate(highest_CLR = quantile(CLR,.90)) %>%
+    mutate(highest_CLR = quantile(CLR,clr_thresh)) %>%
     group_by(seqnames) %>%
     filter(CLR >= highest_CLR) %>%
     as.data.frame()
@@ -76,6 +95,71 @@ for (species_name in species_names){
   candidate_genes$species <- species_name
   head(candidate_genes) 
   
-  write.table(candidate_genes, paste("C:/Users/jmcgirr/Documents/matute/candidates/",species_name,"_candidate_genes_top_90_CLR_for_scaffold_unique_proteins.txt",sep = ""), row.names = FALSE, quote= FALSE,sep="\t")
+  write.table(candidate_genes, paste(species_name,"_candidate_genes.txt",sep = ""), row.names = FALSE, quote= FALSE,sep="\t")
 }
+
+
+#####
+#######################################################
+# 3. UpsetR plot showing shared genes under selection #
+#######################################################
+
+mybiglist <- list()
+for (species_name in species_names){
+
+  cans <- read.delim(paste(species_name,"_candidate_genes.txt",sep = ""), header = TRUE, stringsAsFactors = FALSE, sep = "\t")
+  cans <- cans$Protein.name
+  cans <- str_remove(cans, "conserved hypothetical protein")
+  cans <- str_remove(cans, "predicted protein")
+  cans2 <- unique(cans)
+  mybiglist[[species_name]] <- cans
+}
+
+
+#png("upsetr.png", width = 12, height = 7, units = 'in', res = 600)
+
+upset(fromList(mybiglist), sets = species_names,order.by = "freq", point.size = 3.4, line.size = 1.2, 
+      mainbar.y.label = "genes intersecting", sets.x.label = "genes", nsets = 6,
+      text.scale = c(2, 1.6, 1.5, 1.4, 2,1.6))
+#dev.off()
+
+#####
+##################################################
+# 4. plot CLR for scaffolds with candidate genes #
+##################################################
+
+plot_candidate <- function(focal_species_name,protein_product){
+  
+  sweed <- read.table(paste("SweeD_Report.",focal_species_name,"_grid1000_sweeps.txt",sep = ""), header = FALSE, stringsAsFactors = FALSE)
+  names(sweed) <- c("seqnames","start","CLR","alpha")
+  sweed$start <- round(sweed$start)
+  
+  # set CLR_window_size to remove small scaffolds.
+  # chroms split into 1000 equal chunks, so CLR_window_size > 100 filters chroms smaller than 100kb
+  sweed <- sweed %>% 
+    group_by(seqnames) %>%
+    mutate(CLR_window_size = c(0,diff(start))) %>% filter(CLR_window_size>100) %>%
+    as.data.frame()
+  unique(paste(sweed$seqnames, sweed$CLR_window_size, sep = ":"))
+  sweed$end <- sweed$start + sweed$CLR_window_size
+  sweed <- sweed[c("seqnames","start","end","CLR","alpha","CLR_window_size")]
+  
+  cans <- read.delim(paste(focal_species_name,"_candidate_genes.txt",sep = ""), header = TRUE, stringsAsFactors = FALSE, sep = "\t")
+  cans <- cans %>% filter(Protein.name == protein_product) 
+  range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+  
+  sw <- filter(sweed, seqnames == cans$seqnames[1])
+  smooth_sw <-   smooth.spline(sw$start, range01(sw$CLR), spar = .2)
+  
+  #png(paste(species_name,"_",protein_product,".png",sep = ""), width = 7, height = 4, units = 'in', res = 300)
+  plot(sw$start, range01(sw$CLR), xlab = sw$seqnames[1], main = paste(paste(focal_species_name,cans$Protein.product[1],sep = " "),protein_product, sep = "\n"),ylab = "CLR")#,col = "white")
+  lines(smooth_sw, lwd = 2, col = "#E8000B")
+  sw <- sw[order(sw$CLR, decreasing = TRUE),]
+  abline(v=sw$start[1] , col="#1f77b4", lwd=1, lty=2)
+  #dev.off()
+  
+}
+
+plot_candidate("Neoformans","streptomycin biosynthesis protein StrI")
+plot_candidate("histoplasmaG186ar","streptomycin biosynthesis protein StrI")
 
